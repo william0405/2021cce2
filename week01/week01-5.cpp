ed85400e2b30de9b9cc12641f3100f10dfcee2f5
陳彥霖 <91170836+william0405@users.noreply.github.com>
2022-02-24 17:28:37 +0800

    Update week01-5.cpp

--- week01/week01-5.cpp
+++ week01/week01-5.cpp
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+char line[2000];
+int main()
+{
+	int sum=0;
+
+	
+		while (scanf("%s",line)==1)
+		{
+		int N=strlen(line);
+		int ans=0;
+		for(int i=0;i<N;i++)
+		{
+			if(line[i]=='2') ans++;
+		}
+		printf("%d\n",ans);
+		sum+=ans;
+	}
+	printf("Total: %d\n",sum);
+	return 0;
+}
 
