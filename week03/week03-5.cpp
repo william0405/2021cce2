2961ea06d93b041b80f12fda936d9a6741ddcaba
陳彥霖 <91170836+william0405@users.noreply.github.com>
2022-03-10 17:43:04 +0800

    Update week03-5.cpp

--- week03/week03-5.cpp
+++ week03/week03-5.cpp
@@ -1,1 +1,26 @@
+char line[2000];
+int ans[256];
+int main()
+{
+	int t=1;
+	while( gets(line) )
+	{
+		if(t>1)printf("\n");
+		for(int c=32;c<128;c++)
+		{
+			ans[c]=0;
+		}
+		for(int i=0;line[i]!=0;i++)
+		{
+			char c= line[i];
+			ans[c]++;
+		}
+		for(int c=32;c<128;c++)
+		{
+			if(ans[c]>0)printf("%d %d\n",c,ans[c]);
+		}
+		//printf("第%d筆資料\n",t);
+		t++;
+	}
+}
 
