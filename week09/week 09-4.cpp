00101223320dfdf8b9a6d07747f1919c2910d60b
陳彥霖 <91170836+william0405@users.noreply.github.com>
2022-04-21 17:45:10 +0800

    Update week 09-4.cpp

--- week09/week 09-4.cpp
+++ week09/week 09-4.cpp
@@ -1,1 +1,29 @@
+#include <stdio.h>
+char name[100][80];
+int grade[100];
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		scanf("%s %d",name[i],&grade[i]);
+	}
+	for(int k=0;k<n-1;k++)
+	{
+		for(int i=0;i<n-1;i++)
+		{
+			if(grade[i]<grade[i+1])
+			{
+				int t=grade[i];
+				grade[i]=grade[i+1];
+				grade[i+1]=t;
+			}
+		}
+	}
+	for(int i=0;i<n;i++)
+	{
+		printf("%s %d\n",name[i],grade[i]);
+	}
+}
 
